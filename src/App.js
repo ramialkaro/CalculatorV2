27d9915b069d9ddeea187cc327b5adfe3e644c96
ramialkaro <rami.alkaro@gmail.com>
2021-05-06 17:18:26 +0300

    update the calculator to be more like normal one

--- src/App.js
+++ src/App.js
@@ -1,14 +1,66 @@
-import React from 'react';
-import KeyPad from './Components/Keys';
+import React, {Component} from 'react';
+import Keypad from './Components/Keys';
+import Button from './Components/Button';
+import Display from './Components/Display';
+import "./index.css";
+  
+class Calculator extends Component {
+    constructor() {
+        super();
+        this.state = { data: ''}
+    }
 
-function App() {
-  return (
-    <div>
-      <h3>Calculator Version Two</h3>
+    calculate = () => {
+        try {
+            const result = eval(this.state.data);
+            this.setState({data: result});
+        } catch (e) {
+            this.setState({data: 'error'})
+        }
+    }
 
-      <KeyPad/>     
+    handleClick = e => {
+        const value = e.target.getAttribute('data-value');
+        switch(value) {
+            case 'clear':
+                this.setState({ data: ''});
+                break;
+            case 'equal':
+                this.calculate();
+                break;
+            default:
+                this.setState({ data: this.state.data + value});
+        }
+    }
+    render(){
+return(
+    <div className="Calculator">
+       <Display data={this.state.data}/>
+                <Keypad>
+                    <Button onClick={this.handleClick} label="C" value="clear" />
+                    <Button onClick={this.handleClick} label="7" value="7" />
+                    <Button onClick={this.handleClick} label="4" value="4" />
+                    <Button onClick={this.handleClick} label="1" value="1" />
+                    <Button onClick={this.handleClick} label="0" value="0" />
+
+                    <Button onClick={this.handleClick} label="/" value="/" />
+                    <Button onClick={this.handleClick} label="8" value="8" />
+                    <Button onClick={this.handleClick} label="5" value="5" />
+                    <Button onClick={this.handleClick} label="2" value="2" />
+                    <Button onClick={this.handleClick} label="." value="." />
+
+                    <Button onClick={this.handleClick} label="x" value="*" />
+                    <Button onClick={this.handleClick} label="9" value="9" />
+                    <Button onClick={this.handleClick} label="6" value="6" />
+                    <Button onClick={this.handleClick} label="3" value="3" />
+                    <Button onClick={this.handleClick} label="" value="null" />
+
+                    <Button onClick={this.handleClick} label="-" value="-" />
+                    <Button onClick={this.handleClick} label="+" size="2" value="+" />
+                    <Button onClick={this.handleClick} label="=" size="2" value="equal" />
+                </Keypad>
     </div>
   );
-}
+}}
 
-export default App;
+export default Calculator;
